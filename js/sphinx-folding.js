90d44c8b365cdde748e3fbdb90e545b8df9b3cef
Michel Albert <michel@albert.lu>
2013-02-07 16:03:48 +0100

    Folding *all* DTs.
    This should be fine for the default theme.

--- js/sphinx-folding.js
+++ js/sphinx-folding.js
@@ -3,7 +3,10 @@ goog.provide('SphinxFolding');
 goog.require('goog.array');
 goog.require('goog.debug');
 goog.require('goog.debug.Logger');
+goog.require('goog.debug.Console');
 goog.require('goog.dom');
+goog.require('goog.events');
+goog.require('goog.style');
 
 
 /**
@@ -19,11 +22,26 @@ SphinxFolding.prototype.init = function() {
   var debugWindow = new goog.debug.FancyWindow('main');
   debugWindow.setEnabled(true);
   debugWindow.init();
+  goog.debug.Console.autoInstall();
 
-  var classes = goog.dom.getElementsByTagNameAndClass('dl', 'class');
+  var classes = goog.dom.getElementsByTagNameAndClass('dt');
   goog.array.forEach(classes, function(obj){
-    SphinxFolding.LOG.info(goog.debug.expose(obj));
+    var container = goog.dom.getNextElementSibling(obj);
+    if (goog.isDefAndNotNull(container)) {
+      goog.style.showElement(container, false);
+    }
+    goog.events.listen(obj, goog.events.EventType.CLICK, function(evt) {
+      SphinxFolding.toggleVisibility(this);
+    });
   });
+};
+
+
+SphinxFolding.toggleVisibility = function(obj) {
+  SphinxFolding.LOG.finer("Toggling visibility of sibling of " + obj.tagName);
+  var container = goog.dom.getNextElementSibling(obj);
+  SphinxFolding.LOG.fine("Sibling is " + container);
+  goog.style.showElement(container, !goog.style.isElementShown(container));
 };
 
 
